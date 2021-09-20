2c11f5762c47dc9335266da947f143af62bb1d17
Divine <dchelladurai@vmware.com>
2021-09-20 20:40:58 +0530

    getcurrentLocation changes

--- app/src/main/java/com/tadvice/tripadvisor/FirstFragment.kt
+++ app/src/main/java/com/tadvice/tripadvisor/FirstFragment.kt
@@ -1,14 +1,23 @@
 package com.tadvice.tripadvisor
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.Status
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.PlaceLikelihood
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
+import com.google.android.libraries.places.api.net.PlacesClient
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import com.tadvice.tripadvisor.databinding.FragmentFirstBinding
@@ -27,6 +36,7 @@ class FirstFragment : Fragment() {
 
     private var _binding : FragmentFirstBinding? = null
     private val binding get() = _binding!!
+    private lateinit var placesClient : PlacesClient;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,7 +49,7 @@ class FirstFragment : Fragment() {
         Places.initialize(requireContext(),getString(R.string.google_maps_key))
 
         // Create a new PlacesClient instance
-        val placesClient = Places.createClient(requireContext())
+        placesClient = Places.createClient(requireContext())
 
         // Initialize the AutocompleteSupportFragment.
         val autocompleteFragment =
@@ -48,7 +58,7 @@ class FirstFragment : Fragment() {
 
         // Specify the types of place data to return.
         autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
-
+        autocompleteFragment.setTypeFilter(TypeFilter.ADDRESS)
         // Set up a PlaceSelectionListener to handle the response.
         autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
             override fun onPlaceSelected(place: Place) {
@@ -61,9 +71,60 @@ class FirstFragment : Fragment() {
                 Log.i("FirstFragment", "An error occurred: $status")
             }
         })
+
+        getCurrentLocation();
         return binding.root;
     }
 
+    fun getCurrentLocation(){
+
+        Log.d(
+            "FirstFragment","in getCurrentLocation")
+        // Use fields to define the data types to return.
+        val placeFields: List<Place.Field> = listOf(Place.Field.NAME)
+
+// Use the builder to create a FindCurrentPlaceRequest.
+        val request: FindCurrentPlaceRequest = FindCurrentPlaceRequest.newInstance(placeFields)
+
+// Call findCurrentPlace and handle the response (first check that the user has granted permission).
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) ==
+            PackageManager.PERMISSION_GRANTED) {
+
+            val placeResponse = placesClient.findCurrentPlace(request)
+            placeResponse.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val response = task.result
+                    for (placeLikelihood: PlaceLikelihood in response?.placeLikelihoods ?: emptyList()) {
+                        Log.d(
+                            "FirstFragment",
+                            "Place '${placeLikelihood.place.name}' has likelihood: ${placeLikelihood.likelihood}"
+                        )
+                    }
+                } else {
+                    val exception = task.exception
+                    if (exception is ApiException) {
+                        Log.e("FirstFragment", "Place not found: ${exception.statusCode}")
+                    }
+                }
+            }
+        } else {
+            // A local method to request required permissions;
+            // See https://developer.android.com/training/permissions/requesting
+            //getLocationPermission()
+            val requestMultiplePermissions = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                permissions.entries.forEach {
+                    Log.d("FirstFragment", "${it.key} = ${it.value}")
+
+                }
+            }
+            requestMultiplePermissions.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            )
+        }
+
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null;
